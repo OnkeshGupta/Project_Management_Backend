50fe65d67f3757e2144ff63bf0382304dccbb097
Onkesh Gupta <onkeshgupta@gmail.com>
2025-09-29 23:59:47 +0530

    register user

--- src/app.js
+++ src/app.js
@@ -16,6 +16,10 @@ app.use(cors({
     allowedHeaders: ["Authorization", "Content-Type"]
 }));
 
+import healthCheckRouter from "./routes/healthcheck.routes.js";
+
+app.use("/api/v1/healthcheck", healthCheckRouter);
+
 app.get("/", (req, res) => {
     res.send("Hello world");
 });
